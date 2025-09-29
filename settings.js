3df4c0715c3b014fe7ebfa16eba495c592c3d3a2
Feranbanks <feranbanksrichie6@gmail.com>
2025-09-29 03:43:01 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "ʟᴀᴅʏ ʙᴇʟʟᴀ ᴠ3",
   botOwner: 'sɴᴏᴡʙɪʀᴅ', // Your name
-  ownerNumber: '263780145644', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2347031872859', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
